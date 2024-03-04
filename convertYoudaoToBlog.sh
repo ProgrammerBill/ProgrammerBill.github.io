bad39769121d67709fa9de9cc1adf093bf8bc457
bill <cjcbill@gmail.com>
2024-03-04 13:24:16 +0800

    add new blog by auto robot

--- convertYoudaoToBlog.sh
+++ convertYoudaoToBlog.sh
@@ -12,7 +12,6 @@ BLOG_REPOSITORY_PATH="$HOME/GitHubs/ProgrammerBill.github.io"
 INPUT_MD_PATH="$BLOG_REPOSITORY_PATH/youdao_posts"
 OUTPUT_BLOG_PATH="$BLOG_REPOSITORY_PATH/_posts/"
 ADD_YAML_HEADER_PY="$BLOG_REPOSITORY_PATH/addYamlHeader.py"
-DATE=`date +%Y-%m-%d`
 INPUT_DIR=("blog" "plan" "life")
 
 echo "pulling markdown files..."
@@ -25,6 +24,7 @@ find $INPUT_MD_PATH/${INPUT_DIR[0]} -type f -name "*.md" | while read file; do
     # 在这里处理每个文件
     title_name=$(basename "$file" .md)
     result=$(find $OUTPUT_BLOG_PATH/${INPUT_DIR[0]} -type f -name "*$title_name*")
+    DATE=`date +%Y-%m-%d`
     output_name="$DATE-$title_name.md"
     # 检查变量内容
     if [ -z "$result" ]; then
@@ -49,6 +49,7 @@ find $INPUT_MD_PATH/${INPUT_DIR[1]} -type f -name "*.md" | while read file; do
     echo "Processing $file"
     title_name=$(basename "$file" .md)
     result=$(find $OUTPUT_BLOG_PATH/${INPUT_DIR[1]} -type f -name "*$title_name*")
+    DATE=`date +%Y-%m-%d`
     output_name="$DATE-$title_name.md"
     # 检查变量内容
     if [ -z "$result" ]; then
